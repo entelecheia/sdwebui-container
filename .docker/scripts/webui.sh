b5b734b35a2a6aca696584294b9d952b8e2604ba
Young Joon Lee <entelecheia@hotmail.com>
2023-06-10 17:56:12 +0900

    feat(webui): add support for custom data directory

--- .docker/scripts/webui.sh
+++ .docker/scripts/webui.sh
@@ -7,7 +7,7 @@
 install_dir=${SDW_WORKSPACE_ROOT:-"/app"}
 clone_dir=${SDW_CLONE_DIR:-"stable-diffusion-webui"}
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-export COMMANDLINE_ARGS="--share --listen --enable-insecure-extension-access --xformers"
+export COMMANDLINE_ARGS="--share --listen --enable-insecure-extension-access --xformers --data-dir ./workspace"
 # python3 executable
 python_cmd="python3"
 # git executable
